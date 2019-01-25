5f4ed5abef056acbaa288102655f34c272df404f
Admin <admin@Jacobs-MacBook-Air.local>
2019-01-24 18:38:45 -0800

    fixed typo in client Dockerfile

--- client/bin/run-prod.sh
+++ client/bin/run-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -f Dockerfile -t chrisalehman/fibonacci-app-client .
-docker run -p 8080:8080 -d chrisalehman/fibonacci-app-client
+docker run -p 8080:3000 -d chrisalehman/fibonacci-app-client
